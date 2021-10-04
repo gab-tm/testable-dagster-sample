6faab615ea720c5ce04a95f7ddfd0b1c56132d6e
Gab Gonzales <gab@thinkingmachin.es>
2021-10-04 21:56:49 +0800

    Remove indentation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-  #!/bin/sh
+#!/bin/sh
 docker-compose -f docker-compose-dev.yaml down -v
 docker-compose -f docker-compose-dev.yaml build --no-cache
 docker-compose -f docker-compose-dev.yaml up -d
