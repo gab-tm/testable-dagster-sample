586a1bdb1677812f22cd0976d0289e9152edd6e3
Gab Gonzales <gab@thinkingmachin.es>
2021-10-06 00:13:02 +0800

    Add cloud biuld file for pr merged scenario

--- cool-beans/cool_beans_tests/test_pipelines/test_dumb_encryption_pipeline.py
+++ cool-beans/cool_beans_tests/test_pipelines/test_dumb_encryption_pipeline.py
@@ -8,5 +8,5 @@ def test_my_pipeline():
     assert result.success
     assert (
         result.output_for_solid("leet")
-        == "4_v3rY_L0Ng_57r1nG_W17h_n0_5p4c35_4Nd_5P3C14l_cH4R4C73r5_C0N51d3r3d_45_0n3_W0Rd fdasfdsf"
+        == "4_v3rY_L0Ng_57r1nG_W17h_n0_5p4c35_4Nd_5P3C14l_cH4R4C73r5_C0N51d3r3d_45_0n3_W0Rd"
     )
